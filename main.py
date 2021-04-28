d50ac185f6c9d322b5db770b746adcfbfc82fb83
Artem <artemis.gorb.14@gmail.com>
2021-04-28 21:28:18 +0300

    test

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ TOKEN = str(os.environ.get('TgVKBotToken'))
 def generate_token():
     vk_session = vk_api.VkApi(f'{LOGIN}', f'{PASSWORD}')
     vk_session.auth()
-    return dict(vk_session.token).get('access_token')
+    return "dict(vk_session.token).get('access_token')"
 
 
 def copy_msg(bot: Bot, update: Update):
