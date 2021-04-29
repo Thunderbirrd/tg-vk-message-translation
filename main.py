a9c686364164962234bbdcae42657cbd01166475
Artem <artemis.gorb.14@gmail.com>
2021-04-29 13:03:56 +0300

    fix

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from telegram import Bot, Update
 from telegram.ext import Updater, MessageHandler, Filters
 import requests
 from token import generate_token
-import logging
 
-logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger = logging.getLogger()
 
 LOGIN = str(os.environ.get('login'))
 PASSWORD = str(os.environ.get('password'))
@@ -23,27 +19,21 @@ def copy_msg(bot: Bot, update: Update):
                            f"message={text}&access_token={token}&v=5.130")
     request.close()
     data = request.json()
-    logger.info(data)
 
 
 def main():
-    try:
-        bot = Bot(
-            token=TOKEN,
-        )
-        updater = Updater(bot=bot)
-        message_handler = MessageHandler(Filters.text, copy_msg)
-        updater.dispatcher.add_handler(message_handler)
+    bot = Bot(
+        token=TOKEN,
+    )
+    updater = Updater(bot=bot)
+    message_handler = MessageHandler(Filters.text, copy_msg)
+    updater.dispatcher.add_handler(message_handler)
 
-        updater.start_webhook(listen="0.0.0.0",
-                              port=int(PORT),
-                              url_path=TOKEN)
-        updater.bot.setWebhook(f'https://tg-vk-message.herokuapp.com/{TOKEN}')
-        updater.idle()
-    except Exception as e:
-        logger.error(type(e))
-        logger.error(e.args)
-        logger.error(e)
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook(f'https://tg-vk-message.herokuapp.com/{TOKEN}')
+    updater.idle()
 
 
 if __name__ == '__main__':
