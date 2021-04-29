1ea1bf06b39ff5cafba01e21e1fad5f9e334b5b7
Artem <artemis.gorb.14@gmail.com>
2021-04-29 13:06:09 +0300

    fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Bot, Update
 from telegram.ext import Updater, MessageHandler, Filters
 import requests
-from token import generate_token
+import vk_api
 
 
 LOGIN = str(os.environ.get('login'))
@@ -12,13 +12,18 @@ PORT = int(os.environ.get('PORT', 8443))
 TOKEN = str(os.environ.get('TgVKBotToken'))
 
 
+def generate_token(login, password):
+    vk_session = vk_api.VkApi(login, password)
+    vk_session.auth()
+    return dict(vk_session.token).get('access_token')
+
+
 def copy_msg(bot: Bot, update: Update):
     text = update.channel_post.text
     token = generate_token(LOGIN, PASSWORD)
     request = requests.get(f"https://api.vk.com/method/wall.post?owner_id={OWNER_ID}&from_group=1&"
                            f"message={text}&access_token={token}&v=5.130")
     request.close()
-    data = request.json()
 
 
 def main():
--- token.py
+++
@@ -1,7 +0,0 @@
-import vk_api
-
-
-def generate_token(login, password):
-    vk_session = vk_api.VkApi(login, password)
-    vk_session.auth()
-    return dict(vk_session.token).get('access_token')
